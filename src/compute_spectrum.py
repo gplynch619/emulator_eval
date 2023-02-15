9f705c70e05cac0c844f89f2170536cc57df1d5c
Gabe Lynch <fireballs619@gmail.com>
2023-02-15 12:21:02 -0800

    removed subprocess after memory leak fix

--- src/compute_spectrum.py
+++ src/compute_spectrum.py
@@ -1,11 +1,15 @@
 import numpy as np
 import classy as Class
 import sys
+import os
+import resource
 import pickle
+import psutil
 
 settings_dict_filename = sys.argv[1]
 models_filename = sys.argv[2]
 outfiles_filename = sys.argv[3]
+rank = int(sys.argv[4])
 
 with open(settings_dict_filename, 'rb') as f:
     settings= pickle.load(f)
@@ -32,15 +36,27 @@ try:
         #    process = psutil.Process(os.getpid())
         #    f.write("EB size: {0} KB \t Total mem used: {1} KB\n".format(sys.getsizeof(M)/1000., process.memory_info().rss/1000))
             ### end debugging
+    mem_self = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+    with open("log/r_c{}.log".format(rank), "a") as f:
+        f.write("{} {}\n".format(os.getpid(), mem_self/1000))
+
     M.struct_cleanup()
     M.empty()
+    sys.exit(0)
 
 except Class.CosmoComputationError as failure_message:
     print("Mode failed ")
     print(str(failure_message)+'\n')
     M.struct_cleanup()
-    M.empty() 
+    M.empty()
+    sys.exit(1)
 except Class.CosmoSevereError as critical_message:
     print("Something went wrong when calling CLASS" + str(critical_message))
     M.struct_cleanup()
     M.empty()
+    sys.exit(1)
+
+#with open("log_sub.txt", "a") as f:
+#    process = psutil.Process(os.getpid())
+#    f.write("Total subproc mem used: {} KB\n".format(process.memory_info().rss/1000))
--- src/generate_data.py
+++ src/generate_data.py
@@ -2,30 +2,20 @@ import numpy as np
 from importlib.machinery import SourceFileLoader
 import sys
 import os
-import psutil
+import resource
 import subprocess
 import pickle
 import time
+import classy as Class
 from mpi4py import MPI
 from scipy.stats import qmc
 import itertools
 
 ######################################################
-#  Read parameters
-######################################################
-param_file = sys.argv[1]
-param = SourceFileLoader(param_file, param_file).load_module()
-
-input_param_ranges = param.param_ranges
-additional_settings = param.additional_settings
-output_spectra = param.output_spectra
-ll_max = param.ll_max
-
-######################################################
 #  Input parsing
 ######################################################
 
-def parse_input(input_ranges):
+def parse_input(input_ranges, additional_settings):
 
     if("xe_control_points" in input_ranges.keys()):
         modified_rec = True
@@ -47,17 +37,10 @@ def parse_input(input_ranges):
     
     return param_ranges
 
-param_ranges = parse_input(input_param_ranges)
-
-Nparams = len(param_ranges.keys())
-
-N = param.N
-
-
 ######################################################
 #  Create Latin Hypercube of samples
 ######################################################
-def create_models(N, ranges):
+def create_models(N, ranges, Nparams):
     sampler = qmc.LatinHypercube(d=Nparams)
     samples = sampler.random(n=N)
     i=0
@@ -76,128 +59,157 @@ def create_name_mapping(param_names):
         i+=1
     return mapping
 
-######################################################
-#  Set up MPI
-######################################################
+def main():
 
-comm = MPI.COMM_WORLD
-size = comm.Get_size()
-rank = comm.Get_rank()
+    ######################################################
+    #  Read parameters
+    ######################################################
+    param_file = sys.argv[1]
+    param = SourceFileLoader(param_file, param_file).load_module()
 
-Nworkers = size-1
-next_worker = itertools.cycle(np.arange(1, size))
-is_working = {}
-for r in np.arange(size):
-    is_working[str(r)] = False
+    input_param_ranges = param.param_ranges
+    additional_settings = param.additional_settings
+    output_spectra = param.output_spectra
+    ll_max = param.ll_max
 
-pause = 0.1
-short_pause = 1e-4
-######################################################
-#  Compute
-######################################################
-tmp_dir = 'tmp'
-if rank==0:
-    if not os.path.exists(tmp_dir): 
-        os.mkdir(tmp_dir)
+    param_ranges = parse_input(input_param_ranges, additional_settings)
 
-if rank==0:
-    name_mapping = create_name_mapping(param_ranges.keys())
-else:
-    name_mapping = None
+    Nparams = len(param_ranges.keys())
 
-name_mapping = comm.bcast(name_mapping, root=0)
+    N = param.N
+    ######################################################
+    #  Set up MPI
+    ######################################################
 
-if rank==0:
-    np.random.seed(1)
-    data = create_models(N, param_ranges)
-    #print(data)
-    idx = 0
-    while data.shape[0] > idx:
-        target = next(next_worker)
-        if comm.iprobe(target):
-            _ = comm.recv(source=target)
-            comm.send(data[idx], dest=target)
-            idx+=1
-            is_working[str(r)] = 1
-        else:
-            is_working[str(r)] = 0
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
 
-        if all(value == 0 for value in is_working.values()):
-            time.sleep(pause)
-        else:
-            time.sleep(short_pause)
-        if(idx%1000==0):
-            print("{} have been sent".format(idx))
+    Nworkers = size-1
+    next_worker = itertools.cycle(np.arange(1, size))
+    is_working = {}
+    for r in np.arange(size):
+        is_working[str(r)] = False
 
-    for i in np.arange(1, size):
-        comm.send("done", dest=i)
+    pause = 0.1
+    short_pause = 1e-4
 
-if rank!=0:
-    settings = {}
-    common_settings = {'output' : 'tCl,pCl,lCl',
-                   'thermodynamics_verbose': 0,
-                   'input_verbose': 0,
-                   'lensing': 'yes',
-                   'xe_pert_type': 'none'
-                  }
-    pr_cover_tau = 0.004
-    precision_settings = {"start_sources_at_tau_c_over_tau_h": pr_cover_tau}
+    ######################################################
+    #  Compute
+    ######################################################
 
-    outfiles = {}
-    for xx in output_spectra:
-        outfiles[xx] = os.path.join(param.outdir_root,"{}_spectrum.dat.{}".format(xx, rank))
+    if rank==0:
+        name_mapping = create_name_mapping(param_ranges.keys())
+    else:
+        name_mapping = None
 
-    settings.update(common_settings)
-    settings.update(precision_settings)
-    
-    while True:
-        if len(additional_settings)>0:
-            settings.update(additional_settings)
-        
-        comm.send("waiting for a model", dest=0)
-        model = comm.recv(source=0)
-        if type(model).__name__ == 'str': #breaks when receiving "done" signal
-            break
-        cosmo_settings = {}
-        control_points = []
-        for i,param in enumerate(model):
-            if("q_" in name_mapping[i]):
-                control_points.append(model[i])
+    name_mapping = comm.bcast(name_mapping, root=0)
+    nfailed = np.zeros(1)
+    total_failed = np.zeros(1)
+    if rank==0:
+        #####
+        ### REMEMBER TO TAKE OUT SEED
+        #####
+        np.random.seed(1)
+        data = create_models(N, param_ranges, Nparams)
+        #print(data)
+        idx = 0
+        while data.shape[0] > idx:
+            target = next(next_worker)
+            if comm.iprobe(target):
+                _ = comm.recv(source=target)
+                comm.send(data[idx], dest=target)
+                idx+=1
+                is_working[str(r)] = 1
             else:
-                cosmo_settings[name_mapping[i]] = model[i]
-        if len(control_points)>0:
-            control_points = np.insert(control_points, 0, 0.0)
-            control_points = np.append(control_points, 0.0)
-            str_ctrl = [str(c) for c in control_points]
-            cosmo_settings["xe_control_points"] = ",".join(str_ctrl)
+                is_working[str(r)] = 0
 
-        settings.update(cosmo_settings)
-        settings_dict_filename = os.path.join(tmp_dir, "settings_dict_rank_{}.pkl".format(rank))
-        
-        with open(settings_dict_filename, 'wb') as f:
-            pickle.dump(settings, f)
-        
-        outfiles_dict_filename = os.path.join(tmp_dir,"outfiles_rank_{}.pkl".format(rank))
-        with open(outfiles_dict_filename, 'wb') as f:
-            pickle.dump(outfiles, f)
+            if all(value == 0 for value in is_working.values()):
+                time.sleep(pause)
+            else:
+                time.sleep(short_pause)
+            if(idx%1000==0):
+                print("{} have been sent".format(idx))
 
-        models_filename = os.path.join(tmp_dir,"model_rank_{}.pkl".format(rank))
-        with open(models_filename, 'wb') as f:
-            pickle.dump(model, f)
+        for i in np.arange(1, size):
+            comm.send("done", dest=i)
 
-        subprocess.call(["python", "src/compute_spectrum.py", settings_dict_filename, models_filename, outfiles_dict_filename])
+    if rank!=0:
+        settings = {}
+        common_settings = {'output' : 'tCl,pCl,lCl',
+                    'thermodynamics_verbose': 0,
+                    'input_verbose': 0,
+                    'lensing': 'yes',
+                    'xe_pert_type': 'none'
+                    }
+        pr_cover_tau = 0.004
+        precision_settings = {"start_sources_at_tau_c_over_tau_h": pr_cover_tau}
 
-        #with open("log_{}.txt".format(rank), "a") as f:
-        #    process = psutil.Process(os.getpid())
-        #    f.write("Total mem used: {} KB\n".format(process.memory_info().rss/1000))
-    #done
+        outfiles = {}
+        for xx in output_spectra:
+            outfiles[xx] = os.path.join(param.outdir_root,"{}_spectrum.dat.{}".format(xx, rank))
 
-comm.Barrier()
+        settings.update(common_settings)
+        settings.update(precision_settings)
 
-if rank==0:
-    for filename in os.listdir(tmp_dir):
-        os.remove(os.path.join(tmp_dir, filename))
-    os.rmdir(tmp_dir)
+        while True:
+            if len(additional_settings)>0:
+                settings.update(additional_settings)
+            
+            comm.send("waiting for a model", dest=0)
+            model = comm.recv(source=0)
+            if type(model).__name__ == 'str': #breaks when receiving "done" signal
+                break
+            cosmo_settings = {}
+            control_points = []
+            for i,param in enumerate(model):
+                if("q_" in name_mapping[i]):
+                    control_points.append(model[i])
+                else:
+                    cosmo_settings[name_mapping[i]] = model[i]
+            if len(control_points)>0:
+                control_points = np.insert(control_points, 0, 0.0)
+                control_points = np.append(control_points, 0.0)
+                str_ctrl = [str(c) for c in control_points]
+                cosmo_settings["xe_control_points"] = ",".join(str_ctrl)
 
-MPI.Finalize()
-sys.exit(0)
+            settings.update(cosmo_settings)
+
+            M = Class.Class()
+            M.set(settings)
+
+            try:
+                M.compute()
+                success=True
+            
+            except Class.CosmoComputationError as failure_message:
+                print("Mode failed ")
+                print(str(failure_message)+'\n')
+                success=False
+            
+            except Class.CosmoSevereError as critical_message:
+                print("Something went wrong when calling CLASS" + str(critical_message))
+                success=False
+
+            if success:
+                for xx in output_spectra:
+                    spectrum = M.lensed_cl(ll_max)[xx][2:]
+                    out_array = np.hstack((model, spectrum))
+                    with open(outfiles[xx], 'ab') as f:
+                        np.savetxt(f, [out_array])
+            else:
+                nfailed[0]+=1
+            
+            M.struct_cleanup()
+
+        #done
+    comm.Reduce(nfailed, total_failed, MPI.SUM, 0)
+    if(rank==0):
+        print("{0}/{1} models succeeded".format(len(data)-total_failed[0], len(data)))
+    comm.Barrier()
+
+    MPI.Finalize()
+    sys.exit(0)
+
+if __name__=="__main__":
+    main()
